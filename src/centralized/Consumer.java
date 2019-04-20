8598490537f6b6bf728ec17e788c4ee357e7a74e
lucasebs <lucasebatista@gmail.com>
2019-04-20 11:00:43 -0300

    Write Processing Time per Image ( Tempo de Processamento por Imagem )

--- src/centralized/Consumer.java
+++ src/centralized/Consumer.java
@@ -2,8 +2,7 @@ package centralized;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
@@ -30,42 +29,57 @@ public class Consumer implements  Runnable {
 
         ProcessadorImagens proc = new ProcessadorImagens();
 
-        while(true) {
-            try {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter("src/output/centralized/log/pti_tpi.txt", true));
 
-//                System.out.println("{ Consumer : Block }");
-                block.acquire();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+            while(true) {
+                try {
+
+    //                System.out.println("{ Consumer : Block }");
+                    block.acquire();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                this.img = buffer.remove();
+                if (this.img.isEnd()){
+                    System.out.println("{ Consumer - Done }");
+    //                tempoMedio =
+                    break;
+                }
+
+                free.release();
+    //            System.out.println("{ Consumer : Free }");
+    //            System.out.println(" - Get image '" + this.img.getFile_name() + this.img.getBrilho() +  "' from Buffer...");
+
+                long begin = System.currentTimeMillis();
+                BufferedImage img_out = proc.brilho(this.img.getImg(), this.img.getBrilho());
+    //            System.out.println(" - Image '" + this.img.getFile_name() + this.img.getBrilho() +  "' processed...");
+                long end = System.currentTimeMillis();
+                this.tempos.add(end-begin);
+
+                System.out.println( "Processing Time per Image / Tempo de Processamento por Imagem" );
+                System.out.println( this.tempos.get(this.tempos.size() - 1) + " Milliseconds / Milissegundos");
+
+
+                writer.write(String.valueOf(this.tempos.get(this.tempos.size() - 1)));
+                writer.newLine();
+
+
+                try {
+
+                    File f = new File("src/output/centralized/images/" + this.img.getFile_name() + this.img.getBrilho() + ".jpg");
+                    ImageIO.write(img_out, "jpg", f);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+    //            System.out.println(" - Image '" + this.img.getFile_name() + this.img.getBrilho() +  "' saved...");
             }
 
-            this.img = buffer.remove();
-            if (this.img.isEnd()){
-                System.out.println("{ Consumer - Done }");
-//                tempoMedio =
-                break;
-            }
-
-            free.release();
-//            System.out.println("{ Consumer : Free }");
-//            System.out.println(" - Get image '" + this.img.getFile_name() + this.img.getBrilho() +  "' from Buffer...");
-
-            long begin = System.currentTimeMillis();
-            BufferedImage img_out = proc.brilho(this.img.getImg(), this.img.getBrilho());
-//            System.out.println(" - Image '" + this.img.getFile_name() + this.img.getBrilho() +  "' processed...");
-            long end = System.currentTimeMillis();
-            this.tempos.add(end-begin);
-            System.out.println( "Processing Time per Image / Tempo de Processamento por Imagem" );
-            System.out.println( this.tempos.get(this.tempos.size() - 1) + " Milliseconds / Milissegundos");
-
-            try {
-
-                File f = new File("src/output/centralized/" + this.img.getFile_name() + this.img.getBrilho() + ".jpg");
-                ImageIO.write(img_out, "jpg", f);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//            System.out.println(" - Image '" + this.img.getFile_name() + this.img.getBrilho() +  "' saved...");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
