8fe3ea103ddde558f175daa372a82a40b409c9fb
lucasebs <lucasebatista@gmail.com>
2019-04-21 22:56:59 -0300

    Adjust path to images

--- src/distributed/client/PrincipalClient.java
+++ src/distributed/client/PrincipalClient.java
@@ -3,15 +3,9 @@ package distributed.client;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PrincipalClient {
 
@@ -21,8 +15,19 @@ public class PrincipalClient {
 
         BufferedReader f = new BufferedReader(new FileReader("src/input/servers.txt"));
 
+
+        ArrayList<String> result = new ArrayList<String>();
+
+        File folder = new File("src/input/video/");
+        File[] listOfFiles = folder.listFiles();
+
+        for (int j = 0; j < listOfFiles.length; j++) {
+            result.add(listOfFiles[j].getName());
+        }
+
         int i = 0;
         String line;
+        long begin = System.currentTimeMillis();
         while ((line = f.readLine()) != null) {
             System.out.println(line);
             String[] info = line.split(",");
@@ -31,43 +36,24 @@ public class PrincipalClient {
             sockets.add(s);
 
             DataInputStream input = new DataInputStream(s.getInputStream());
-
-            ImageReceiverClient imageReceiverClient = new ImageReceiverClient(input);
+//            System.out.println(listOfFiles.length);
+            ImageReceiverClient imageReceiverClient = new ImageReceiverClient(input, begin);
             Thread t = new Thread(imageReceiverClient);
             t.start();
             i++;
         }
-        System.out.println("Enviando Imagem...");
-
-        List<String> result = null;
-
-        try (Stream<Path> walk = Files.walk(Paths.get("src/input/video/"))) {
-
-            result = walk.filter(Files::isRegularFile)
-                    .map(x -> x.toString()).collect(Collectors.toList());
-
-//            result.forEach(System.out::println);
-
-            //                System.out.println(r);
-//            files.addAll(result);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
+        System.out.println("Sending images...");
         int cont = 0;
 //        for (int j = 0; j<=5; j++) {
         for (String r : result) {
-            System.out.println(r);
+            System.out.println("- " + r);
 //            System.out.println("Enviando Imagem "+j+" para socket "+cont % sockets.size()+"...");
             Thread.sleep(1000);
             cont++;
             DataOutputStream output = new DataOutputStream(sockets.get(cont % sockets.size()).getOutputStream());
 
 //            File f1 = new File("src/input/video/3840_RIGHT_0212"+j+".jpg");
-            File f1 = new File(r);
+            File f1 = new File("src/input/samples/" + r);
 
             InputStream in = new FileInputStream(f1);
 
@@ -77,14 +63,15 @@ public class PrincipalClient {
 
             //enviando a quantidade de bytes do arquivo
             output.writeLong(length);
-            //enviadno nome do arquivo
-            output.writeUTF(r);
-            //enviando o arquivo pela rede
+            //enviando o nome do arquivo
+            output.writeUTF(r.split("\\.")[0]);
+            //enviando o brilho a ser aplicado
             output.writeInt(100);
+            //enviando o arquivo pela rede
             int count = 0;
             //enquanto houver bytes para enviar, obtém do arquivo e manda pela rede
             while ((count = in.read(bytes, 0, bytes.length)) > 0) { //count recebe a qtd de bytes lidos do arquivo para serem enviados
-                System.out.println("Enviou " + count + " bytes");
+//                System.out.println("Enviou " + count + " bytes");
                 try {
                     output.write(bytes, 0, count); //envia count bytes pela rede
                 } catch (IOException e) {
@@ -92,6 +79,7 @@ public class PrincipalClient {
                 }
             }
         }
+//        long end = System.currentTimeMillis();
         cont = 0;
         for (int k = 0; k<=sockets.size(); k++) {
             DataOutputStream output = new DataOutputStream(sockets.get(cont % sockets.size()).getOutputStream());
@@ -99,14 +87,12 @@ public class PrincipalClient {
             output.writeLong(0);
         }
 
-
         f.close();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(PrincipalClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(PrincipalClient.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 }
 
